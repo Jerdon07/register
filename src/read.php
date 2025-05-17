ee35123371c9e71c14b13c99985c727a1ca9344b
Jerdon07 <jdlitaoen@gmail.com>
2025-05-17 09:39:31 +0800

    Initialized the CRUD Files

---
+++ src/read.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accounts</title>
+</head>
+<body>
+    <div>
+        <header>
+            <h3>Users</h3>
+            <button>Add</button>
+            <button>Logout</button>
+        </header>
+
+        <main>
+            
+        </main>
+    </div>
+</body>
+</html>
