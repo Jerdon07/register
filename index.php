ebaac28b6f2c7999c0b2aafa14e4678126434458
Jerdon07 <jdlitaoen@gmail.com>
2025-05-17 09:37:19 +0800

    Initialized the File

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/style.css">
+    <title>Introduction</title>
+</head>
+<body>
+    <div class="container">
+        <h2 class="title">Account Registration</h2>
+        <button class="enter" onclick="window.location.href='./src/read.php';">Enter</button>
+    </div>
+</body>
+</html>
