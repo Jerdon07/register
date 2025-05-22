00dd325ffdfe50451bbb5bd6e01f6b3551fb29b8
Jerdon07 <jdlitaoen@gmail.com>
2025-05-22 21:12:24 +0800

    Initiaized the Delete Front-End

--- src/delete.php
+++ src/delete.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../styles/style.css">
+    <title>Delete User</title>
 </head>
 <body>
-    
+    <div class="container">
+        <h2 class="title">Confirm Deletion</h2>
+
+        <form method="POST" action="read.php">
+            <input type="password">
+            <button>Delete</button>
+        </form>
+        <button class="enter" onclick="window.location.href='./src/read.php';">USERS</button>
+    </div>
 </body>
 </html>
