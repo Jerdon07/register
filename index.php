aa816657adc2c8236056d7be97c5f2f99bcc557f
Jerdon07 <jdlitaoen@gmail.com>
2025-05-25 17:14:08 +0800

    Major Changes

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
     <link rel="stylesheet" href="./styles/style.css">
     <title>Introduction</title>
 </head>
-<body>
+<body class="body-container">
     <div class="container">
         <h2 class="title">Account Registration</h2>
-        <button class="enter" onclick="window.location.href='./src/read.php';">Enter</button>
+        <button class="main-btn" onclick="window.location.href='./src/read.php';">Enter</button>
     </div>
 </body>
 </html>
---
+++ src/public/create.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/style.css">
+    <title>Create User</title>
+</head>
+<body class="body-container">
+    <div class="container">
+        <h2 class="title">CREATE USER</h2>
+        <form method="POST" action="./read.php">
+            <input class="input-box" type="email" name="gmail" placeholder="Enter your email" required>
+            <input class="input-box" type="password" name="password" placeholder="Enter your password" required>
+            <button class="main-btn" onclick="window.location.href='./read.php';">CREATE</button>
+        </form>
+        <a class="login-link" href="./read.php">Already have an Account?</a>
+    </div>
+</body>
+</html>
